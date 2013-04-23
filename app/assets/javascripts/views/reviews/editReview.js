63f14bfe3b875f719361f96fc643f29cde75dd77
dcrelling <dcrelling@paypal.com>
2013-04-22 23:32:31 -0400

    added a few methods to edit and save the model from the form

--- app/assets/javascripts/views/reviews/editReview.js
+++ app/assets/javascripts/views/reviews/editReview.js
@@ -1,10 +1,24 @@
 var EditReview = Backbone.View.extend({
   
+events: {
+    "click button.editButton": "edit"
+    },
+
   render: function () {
         var self = this;
         this.$el.html(JST["reviews/editReview"]({model: this.model}));
         return this;
-    }
+    },
+
+  edit: function(){
+      alert("shit working");
+  }, 
+
+  save: function(){
+
+  } 
+
+
 
 
 

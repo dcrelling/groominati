a18bdfd26c305425dcba09080d6505212b564b4c
dcrelling <dcrelling@paypal.com>
2013-06-07 17:36:32 -0400

    fixed the users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130607202038) do
+ActiveRecord::Schema.define(:version => 20130607213203) do
 
   create_table "activities", :force => true do |t|
     t.integer  "trackable_id"
@@ -89,6 +89,7 @@ ActiveRecord::Schema.define(:version => 20130607202038) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true

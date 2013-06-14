1a058aff46be1e0622dd5cda6428bcacf84c15b8
dcrelling <dcrelling@paypal.com>
2013-06-14 09:39:13 -0400

    some refactoring

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ActiveRecord::Base
   acts_as_followable
   acts_as_follower
 
- # attr_accessible :email, :name, :avatar
+  #attr_accessible :email, :name, :avatar
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 
 end
